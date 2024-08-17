4b68871b715a4f23e3f21d5eb72e3ad7d82b2a35
Adarshjeet0 <adarshjeetc@gmail.com>
2024-08-17 22:15:40 +0530

    Created server to handle multiple request

--- First NodeJs Program/server.js
+++ First NodeJs Program/server.js
@@ -1,14 +1,36 @@
-//create a server using NodeJs.
+//**********Server to handle single request**************
 
-// 1. Import http library/module
+
+// //create a server using NodeJs.
+
+// // 1. Import http library/module
+// const http = require('http');
+
+// // 2. Create server
+// const server = http.createServer((req, res) =>{
+//     res.end('Welcome to my first server.');
+// });
+
+// // 3. Specify a port to listen client's request
+// server.listen(3100, ()=>{
+//     console.log('Server is listening on port number 3100');
+// });
+
+// ***************Server to handle multiple request ****************
+
+// import library/module
 const http = require('http');
-
-// 2. Create server
-const server = http.createServer((req, res) =>{
-    res.end('Welcome to my first server.');
+const server = http.createServer((req, res)=>{
+    if(req.url === '/product'){
+        res.end('Welcome to product page');
+    }else if(req.url === '/user'){
+        res.end('Welcome to User page');
+    }else{
+        res.end('Welcome to Running server')
+    }
 });
 
-// 3. Specify a port to listen client's request
-server.listen(3100, ()=>{
-    console.log('Server is listening on port number 3100');
+
+server.listen(8080, ()=>{
+    console.log("Server is running on port 8080");
 });
