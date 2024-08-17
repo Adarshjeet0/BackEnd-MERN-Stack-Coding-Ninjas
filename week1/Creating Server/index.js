6898e58c7b72a17658f66dc77b3d799e1737b771
Adarshjeet0 <adarshjeetc@gmail.com>
2024-08-17 15:33:16 +0530

    Server created

--- week1/Creating Server/index.js
+++ week1/Creating Server/index.js
@@ -2,5 +2,13 @@
 // Import the necessary modules here
 
 // Write your code here
+const http = require('http');
+const server = http.createServer((req, res)=>{
+    res.end('Response received at port 8080.');
+});
+
+server.listen(8080, ()=>{
+    console.log('Server is listening on port 8080');
+});
 
 module.exports = server;
