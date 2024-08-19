176ee3101fe3d425ae2ec3f5040437c3f9d5c4bc
Adarshjeet0 <adarshjeetc@gmail.com>
2024-08-20 01:54:26 +0530

    Removed mjs extension

---
+++ First NodeJs Program/Removing mjs extension/aritmatic.js
@@ -0,0 +1,14 @@
+// export function add(a, b) {
+//     console.log(`Sum is: ${a+b}`);
+// }
+// export function sub(a,b) {
+//     console.log(`Sum is: ${a-b}`);
+// }
+
+export function add(a, b) {
+    console.log(`Sum is: ${a + b}`);
+}
+
+export function sub(a, b) {
+    console.log(`Difference is: ${a - b}`);
+}
---
+++ First NodeJs Program/Removing mjs extension/result.js
@@ -0,0 +1,4 @@
+import { add,sub } from "./aritmatic.js";
+
+add(2,3);
+//Here we will not use mjs because package.json contains type:module.
